185909300472ea04595ed8ff7ba19e2fe045531d
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-05 01:49:43 +0300

    feat: update Post and User models to include references and improve schema definitions

--- backend/travelDiaries/models/Post.js
+++ backend/travelDiaries/models/Post.js
@@ -1,10 +1,31 @@
-import mongoose, { Schema } from 'mongoose';
-const postSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-  location: { type: String, required: true },
-  date: { type: Date, default: Date.now, required: true },
-  user: { type: String, required: true },
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
 });
+
 export default mongoose.model('Post', postSchema);
--- backend/travelDiaries/models/User.js
+++ backend/travelDiaries/models/User.js
@@ -1,8 +1,22 @@
-import { Schema, model } from 'mongoose';
+import mongoose, { model, Schema } from 'mongoose';
+
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlength: 6 },
-  posts: [{ type: String }],
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  posts: [{ type: mongoose.Types.ObjectId, ref: 'Post' }],
 });
+
 export default model('User', userSchema);
+// users
